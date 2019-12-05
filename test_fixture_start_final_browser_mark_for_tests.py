1c1fddde6c6c3a85a493f62040e1051c70f2d834
Julia Selyutina <jusereda@gmail.com>
2019-12-05 16:27:15 +0300

    Added file "test_fixture_start_final_browser_mark_for_tests.py". File includes fixture for start and finalize browser which will be called for function scope. Also test-functions have custom markers. Marks are defined into "pytest.ini" file.
    For running test with choosen marks use command -m mark_name file_name.py

---
+++ test_fixture_start_final_browser_mark_for_tests.py
@@ -0,0 +1,30 @@
+import time
+import pytest
+from selenium import webdriver
+
+link = 'http://selenium1py.pythonanywhere.com/'
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print('\nstart Firefox for test..')
+    driver = webdriver.Firefox()
+    yield driver
+    print('\nquit Firefox..')
+    driver.quit()
+
+
+class TestFirstPage():
+    @pytest.mark.smoke
+    def test_user_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector('#login_link')
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector('.basket-mini .btn-group > a')
+
+''' In this project registration of marks performs in the file "pytest.ini". Another programmatically way is bellow.
+def pytest_configure(config):
+    config.addinivalue_line("markers", "env(name): mark test to run only on named environment")'''
