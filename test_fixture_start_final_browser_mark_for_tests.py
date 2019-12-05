607c4511e687671f9805d06b51fec8227aae4757
Julia Selyutina <jusereda@gmail.com>
2019-12-05 16:33:55 +0300

    Added new test with xfail mark

--- test_fixture_start_final_browser_mark_for_tests.py
+++ test_fixture_start_final_browser_mark_for_tests.py
@@ -25,6 +25,10 @@ class TestFirstPage():
         browser.get(link)
         browser.find_element_by_css_selector('.basket-mini .btn-group > a')
 
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.find_element_by_css_selector('button.favorite')
+
 ''' In this project registration of marks performs in the file "pytest.ini". Another programmatically way is bellow.
 def pytest_configure(config):
     config.addinivalue_line("markers", "env(name): mark test to run only on named environment")'''
