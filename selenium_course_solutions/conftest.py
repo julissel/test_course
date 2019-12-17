2aa2f620803643ae6c87cbf9ed262001b8460924
Julia Selyutina <jusereda@gmail.com>
2019-12-17 21:18:45 +0300

    Added new file "test_parser.py". It contains simple test. File "conftest.py" was modified. There was added browser selection.

--- selenium_course_solutions/conftest.py
+++ selenium_course_solutions/conftest.py
@@ -2,10 +2,20 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None, help='Chose browser: chrome or firefox')
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Firefox()
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        print("\nstart browser Chrome for test..")
+        browser = webdriver.Chrome()
+    elif browser_name =='firefox':
+        print("\nstart browser Firefox for test..")
+        browser = webdriver.Firefox()
     yield browser
     print("\nquit browser..")
     browser.quit()
