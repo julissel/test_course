6aac830e1b0f83cec887a40835493e350bbd455d
Julia Selyutina <jusereda@gmail.com>
2019-12-17 22:18:09 +0300

    Added description for configuring and using pytest-rerunfailures plugin.

--- selenium_course_solutions/test_parser.py
+++ selenium_course_solutions/test_parser.py
@@ -1,5 +1,16 @@
+'''
+For rerun failed test save in Run/Debug configurations/Additional Arguments: --reruns int_number_of_reruns
+or type in console" pytest -v --reruns int_number_of_reruns
+'''
+
 link = 'http://selenium1py.pythonanywhere.com/'
 
-def test_guest_should_see_login_link(browser):
+
+def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
     browser.find_element_by_css_selector('#login_link')
+
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector('#magic_link')
