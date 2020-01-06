a77117c12fe52fd69c0d64835dc94a652ead7cdb
Julia Selyutina <jusereda@gmail.com>
2020-01-06 10:47:56 +0300

    Added "test_guest_can_go_to_login_page". All files moved to "Page_Objects_design_pattern" directory.

---
+++ Page_Objects_design_pattern/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import  webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose language')
+    parser.addoption('--browser_name', action='store', default='firefox', help='Chose browser: chrome or firefox')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == 'chrome':
+        print('\nstart browser Chrome for test..')
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        print('\nstart browser Firefox for test..')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_language', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    yield browser
+    print('\nquit browser..')
+    browser.quit()
+
