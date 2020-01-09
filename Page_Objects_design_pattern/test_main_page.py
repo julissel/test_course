8b1694d1c190f267d3f501e577bc0ed957ebcb8a
Julia Selyutina <jusereda@gmail.com>
2020-01-09 21:22:32 +0300

    Added transition between pages.

--- Page_Objects_design_pattern/test_main_page.py
+++ Page_Objects_design_pattern/test_main_page.py
@@ -2,15 +2,11 @@ from Page_Objects_design_pattern.pages.main_page import MainPage
 from Page_Objects_design_pattern.pages.login_page import LoginPage
 
 
-def test_guest_can_see_login_link(browser):
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open() # открываем страницу
-    page.should_be_login_link() # проверяем наличие объекта "ссылки" на странице
-
-def test_guest_can_go_to_login_page(browser):
-    # Тестируем страницу логина и регистрации
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    page.go_to_login_page() # переходим на страницу авторизации/регистрации
+    # Тестируем страницу авторизации/регистрации
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
